d6a3d32a36560317c256e34b508138394f74ff4a
grigorovich.kris <kristina.grigorovich98@gmail.com>
2020-10-10 11:34:53 +0300

    First commit

---
+++ ConsoleApplication7.cpp
@@ -0,0 +1,33 @@
+﻿// ConsoleApplication7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+	string s;
+	int k;
+	int main()
+
+
+{
+	
+	cout << "Введите строку (ENG):" << endl;
+	getline(cin, s);
+	k = 0;
+	for (int i = s.length(); i--;)
+	{
+		if (s[i] == '*')
+		{
+			s.erase(i, 1);
+			k++;
+		}
+	}
+	cout << "Строка после удаления:" << endl;
+	cout << s << endl;
+	cout << "Количество удаленных символов: " << k << endl;
+
+	return 0;
+}
+
---
+++ Lab2/Lab2.cpp
@@ -0,0 +1,34 @@
+﻿// Lab2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include <string>
+#include <iostream>
+using namespace std;
+
+int k;
+string s; 
+
+int main()
+{
+	setlocale(LC_ALL, "RUSSIAN");
+	
+	cout << "Введите строку (ru):" << endl;
+	getline(cin, s);
+	k = 0;
+
+	for (int i = s.length(); i--;)
+	{
+		if (s[i] == '*')
+		{
+			s.erase(i, 1);
+			k++;
+		}
+	}
+
+	cout << "Строка после удаления:" << endl;
+	cout << s << endl;
+	cout << "Количество удаленных символов: " << k << endl;
+
+	return 0;
+
+
+};
